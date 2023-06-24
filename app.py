c4ec852e32f6053f54e68d3d3b0d8546dc144d72
elena <elenajeanday@gmail.com>
2023-06-24 22:15:11 +0100

    added login page and fixed the page formatting

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask, render_template
-
+from static.scripts.scripts import * 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  # check login
+  # get current data rates and populate calories n percentages
+  calories, percentages = getToday()
+  return render_template('index.html', calories_left = calories, perc=percentages)
 
 @app.route('/today')
 def today():
   return render_template('today.html')
 
+@app.route('/profile')
+def profile():
+  return render_template('profile.html')
+
+@app.route('/plan')
+def plan():
+  return render_template('mealplan.html')
+
+@app.route('/today/<meal>')
+def meal(meal):
+  return render_template('addmeal.html')
+
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
---
+++ static/scripts/scripts.py
@@ -0,0 +1,2 @@
+def getToday():
+    return 1600, [0,0,0]
